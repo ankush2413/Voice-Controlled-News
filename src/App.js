ce9da9b6da36d4de446e4dad93c9c29487645de0
Ankush Mangal <ankushmangal99@gmail.com>
2022-06-25 12:33:06 +0530

    Basic frontend & API Key set

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useEffect} from 'react';
+import alanBtn from '@alan-ai/alan-sdk-web';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const alanKey = '95d1268845b8f757220ada5380c899f92e956eca572e1d8b807a3e2338fdd0dc/stage';
+
+const App = () =>{
+
+  useEffect(()=>{
+    alanBtn({
+      key: alanKey,
+      onCommand: ({command})=>{
+        if(command === 'testCommand'){
+          alert('This code was');
+        }
+      }
+    })
+  })
+  return(<>
+  <h1>Hello</h1>
+  </>)
 }
 
 export default App;
